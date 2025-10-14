4cec3e94888f49f95435ef2b6746ee0acadedf1b
olyad boka <olyadboka@gmail.com>
2025-10-14 20:37:17 +0300

    updated the model

---
+++ server/models/user_model.js
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    role: {
+      type: String,
+      enum: ["admin", "user", "coach"],
+      default: "user",
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    profileImage: {
+      type: String,
+      default: "",
+    },
+
+    membershipType: {
+      type: String,
+      enum: ["basic", "premium", "vip"],
+      default: "basic",
+    },
+    joinDate: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("User", userSchema);
